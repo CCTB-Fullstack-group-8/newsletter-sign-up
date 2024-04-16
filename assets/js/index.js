7de6437ba7a1baf30e53e83305cd0f57b91fc8b7
kaynan <kaynan.camargo@terceiro-sky.com.br>
2024-04-15 20:06:37 -0700

    feature: create css/js basis code

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+const mainForm = document.querySelector('form');
+const headerSection = document.querySelector('header');
+const formSection = document.querySelector('section');
+const successMessage = document.querySelector('.success-section');
+const container = document.querySelector('.container');
+
+mainForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  handleFormSubmission();
+});
+
+function handleFormSubmission() {
+  displaySuccess();
+}
+
+function displaySuccess() {
+  formSection.style.display = 'none';
+  headerSection.style.display = 'none';
+  successMessage.style.display = 'block';
+  container.style.display = 'none';
+}
