08afc16aceba318edac2fa04f96040b6adbd0bea
kaynan <kaynan.camargo@terceiro-sky.com.br>
2024-04-18 13:00:13 -0700

    fix: submission button

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,7 @@ const emailInput = document.querySelector('.email-form');
 const successEmail = document.querySelector('.success-section .email');
 const dismissButton = document.querySelector('.dismiss-button');
 
-mainForm.addEventListener('submit', function(event) {
+mainForm.addEventListener('submit', function (event) {
   event.preventDefault();
   const email = emailInput.value.trim();
   if (validateEmail(email)) {
@@ -41,10 +41,10 @@ function displaySuccess(email) {
   container.style.display = 'none';
 }
 
-dismissButton.addEventListener('click', function() {
+dismissButton.addEventListener('click', function () {
   // Return to the home page when the user clicks on "Dismiss Message"
   formSection.style.display = 'block';
   headerSection.style.display = 'block';
   successMessage.style.display = 'none';
-  container.style.display = 'block';
+  container.style.display = '';
 });
